b294c12b58d23f2b8ddd17838b463c4836edf528
Danny Lui <dannylui91@gmail.com>
2016-10-30 00:22:29 -0400

    added some animations

--- app/src/main/java/nyc/c4q/dannylui/weatheralpha/fragments/CurrentWeatherFragment.java
+++ app/src/main/java/nyc/c4q/dannylui/weatheralpha/fragments/CurrentWeatherFragment.java
@@ -9,6 +9,11 @@ import android.text.style.AbsoluteSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -96,6 +101,28 @@ public class CurrentWeatherFragment extends Fragment implements UpdateableFragme
         currentFeelView.setVisibility(View.VISIBLE);
         currentWindSpeedView.setVisibility(View.VISIBLE);
         currentRainChanceView.setVisibility(View.VISIBLE);
+
+        setAnimationStuff(currentHiView);
+        setAnimationStuff(currentLoView);
+        setAnimationStuff(currentFeelView);
+        setAnimationStuff(currentWindSpeedView);
+        setAnimationStuff(currentRainChanceView);
+    }
+
+    public void setAnimationStuff(TextView t) {
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
+        fadeIn.setDuration(1000);
+/*
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+        fadeOut.setStartOffset(1000);
+        fadeOut.setDuration(1000);
+*/
+        AnimationSet animation = new AnimationSet(false); //change to false
+        animation.addAnimation(fadeIn);
+        //animation.addAnimation(fadeOut);
+        t.setAnimation(animation);
     }
 
     @Override
